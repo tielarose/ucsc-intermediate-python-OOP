b9e74d145a3071b1168e47470c1d8b4275cbdd90
Tiela Black-Law <tiela.rose@gmail.com>
2024-02-10 21:04:34 -0800

    Add jokers and suit ranking to HigherOrLower game

--- week1-hw/HigherOrLower Non OOP.py
+++ week1-hw/HigherOrLower Non OOP.py
@@ -3,33 +3,62 @@
 import random
 
 # Card constants
-SUIT_TUPLE = ('Hearts', 'Clubs', 'Diamonds', 'Spades', )
-RANK_TUPLE = ('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King')
+SUIT_TUPLE = (
+    "Hearts",
+    "Clubs",
+    "Diamonds",
+    "Spades",
+)
+RANK_TUPLE = (
+    "Ace",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Jack",
+    "Queen",
+    "King",
+    "Joker",
+)
 
 NCARDS = 8
 
+SUITS_RANK = {"Spades": 4, "Diamonds": 3, "Clubs": 2, "Hearts": 1}
+
+
 # Pass in a deck and this function returns a random card from the deck
 def getCard(deckListIn):
-    thisCard = deckListIn.pop() # pops one off the top of the deck and returns
+    thisCard = deckListIn.pop()  # pops one off the top of the deck and returns
     return thisCard
-    
+
+
 # Pass in a deck and this function returns a shuffled copy of the deck
 def shuffle(deckListIn):
     deckListOut = deckListIn.copy()  # make a copy of the starting deck
     random.shuffle(deckListOut)
     return deckListOut
 
+
 #  Main code
-print('Welcome to Higher Or Lower')
-print('You have to choose if the next card to be shown will be higher or lower than the current card.')
-print('Getting it right adds 20 points, get it wrong and you lose 15 points.')
-print('You have 50 points to start.')
+print("Welcome to Higher Or Lower")
+print(
+    "You have to choose if the next card to be shown will be higher or lower than the current card."
+)
+print("Getting it right adds 20 points, get it wrong and you lose 15 points.")
+print("You have 50 points to start.")
 print()
 
 startingDeckList = []
 for suit in SUIT_TUPLE:
     for thisValue, rank in enumerate(RANK_TUPLE):
-        cardDict = {'rank':rank, 'suit':suit, 'value':thisValue + 1}
+        if rank == "Joker" and (suit == "Clubs" or suit == "Diamonds"):
+            continue
+        cardDict = {"rank": rank, "suit": suit, "value": thisValue + 1}
         startingDeckList.append(cardDict)
 
 
@@ -39,58 +68,63 @@ while True:  # play multiple games
     print()
     gameDeckList = shuffle(startingDeckList)
     currentCardDict = getCard(gameDeckList)
-    currentCardRank = currentCardDict['rank']
-    currentCardValue = currentCardDict['value']
-    currentCardSuit = currentCardDict['suit']    
-    print('Starting card is:', currentCardRank + ' of ' + currentCardSuit)
+    currentCardRank = currentCardDict["rank"]
+    currentCardValue = currentCardDict["value"]
+    currentCardSuit = currentCardDict["suit"]
+    print("Starting card is:", currentCardRank + " of " + currentCardSuit)
     print()
 
-    for cardNumber in range(0, NCARDS):   # play one game of this many cards
-        answer = input('Will the next card be higher or lower than the ' + 
-                               currentCardRank + ' of ' + 
-                               currentCardSuit + '?  (enter h or l): ')
+    for cardNumber in range(0, NCARDS):  # play one game of this many cards
+        answer = input(
+            "Will the next card be higher or lower than the "
+            + currentCardRank
+            + " of "
+            + currentCardSuit
+            + "?  (enter h or l): "
+        )
         answer = answer.casefold()  # force lower case
         nextCardDict = getCard(gameDeckList)
-        nextCardRank = nextCardDict['rank']
-        nextCardSuit = nextCardDict['suit']
-        nextCardValue = nextCardDict['value']
-        print('Next card is:', nextCardRank + ' of ' + nextCardSuit)
+        nextCardRank = nextCardDict["rank"]
+        nextCardSuit = nextCardDict["suit"]
+        nextCardValue = nextCardDict["value"]
+        print("Next card is:", nextCardRank + " of " + nextCardSuit)
 
-        if answer == 'h':
+        if answer == "h":
             if nextCardValue > currentCardValue:
-                print('You got it right, it was higher')
+                print("You got it right, it was higher")
+                score = score + 20
+            elif (
+                nextCardValue == currentCardValue
+                and SUITS_RANK[nextCardSuit] > SUITS_RANK[currentCardSuit]
+            ):
+                print("You got it right, it was higher")
                 score = score + 20
             else:
-                print('Sorry, it was not higher')
-                score = score - 15          
+                print("Sorry, it was not higher")
+                score = score - 15
 
-        elif answer == 'l':
+        elif answer == "l":
             if nextCardValue < currentCardValue:
                 score = score + 20
-                print('You got it right, it was lower')
-
+                print("You got it right, it was lower")
+            elif (
+                nextCardValue == currentCardValue
+                and SUITS_RANK[nextCardSuit] < SUITS_RANK[currentCardSuit]
+            ):
+                score = score + 20
+                print("You got it right, it was lower")
             else:
                 score = score - 15
-                print('Sorry, it was not lower')
+                print("Sorry, it was not lower")
 
-        print('Your score is:', score)
+        print("Your score is:", score)
         print()
         currentCardRank = nextCardRank
         currentCardValue = nextCardValue
         currentCardSuit = nextCardSuit
 
     goAgain = input('To play again, press ENTER, or "q" to quit: ')
-    if goAgain == 'q':
+    if goAgain == "q":
         break
 
-print('OK bye')
-
-
-    
-    
-
-
-
-         
-
-    
+print("OK bye")
