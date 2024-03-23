f2d74b935034efe1f4a361a2356f045575bc28f3
Tiela Black-Law <tiela.rose@gmail.com>
2024-03-23 16:54:04 -0700

    Complete Class card

--- week6-hw/HigherOrLower OOP/Card.py
+++ week6-hw/HigherOrLower OOP/Card.py
@@ -2,35 +2,50 @@
 
 import pygwidgets
 
-class Card():
+
+class Card:
 
     def __init__(self, window, rank, suit, value):
         # must add code here to save away parameters in instance variables
         # and create two Image objects, one for the current card, one for the backOfCard
-        pass     # you can remove this line when you add your own
-
-    # You can remove the print statement below
-    # These are just place holders.
-
-
+        self.window = window
+        self.rank = rank
+        self.suit = suit
+        self.value = value
+        self.x = 0
+        self.y = 0
+        self.frontOfCard = pygwidgets.Image(
+            window, (self.x, self.y), f"images/{rank} of {suit}.png"
+        )
+        self.backOfCard = pygwidgets.Image(
+            window, (self.x, self.y), "images/BackOfCard.png"
+        )
+        self.showCard = False
 
     def conceal(self):
-        print('Must conceal the card here')
+        self.showCard = False
 
     def setLoc(self, locTuple):
-        print('Called setLoc method, passed in', locTuple)
+        x, y = locTuple
+        self.x = x
+        self.y = y
+        self.backOfCard.setLoc((self.x, self.y))
+        self.frontOfCard.setLoc((self.x, self.y))
 
     def reveal(self):
-        print('Must reveal card here')
+        self.showCard = True
 
     def getName(self):
-        print('Get the name of the card here')
+        return f"{self.rank} of {self.suit}"
 
     def getValue(self):
-        print('Get the value of the card here')
+        return self.value
 
     def draw(self):
-        print('Draw the card here')
+        if self.showCard:
+            self.frontOfCard.draw()
+        else:
+            self.backOfCard.draw()
 
     # No need to change this
     def getCardNameAndValue(self):
