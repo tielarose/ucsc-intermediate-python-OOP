9b2bf8b66138de2e62961bbfa789d9238d4ea220
Tiela Black-Law <tiela.rose@gmail.com>
2024-03-23 21:34:46 -0700

    Minor refactor based on instructor's notes

--- week6-hw/HigherOrLower OOP/Card.py
+++ week6-hw/HigherOrLower OOP/Card.py
@@ -12,15 +12,7 @@ class Card:
         self.rank = rank
         self.suit = suit
         self.value = value
-        # self.x = 0
-        # self.y = 0
-        # self.frontOfCard = pygwidgets.Image(
-        #     window, (self.x, self.y), f"images/{rank} of {suit}.png"
-        # )
-        # self.backOfCard = pygwidgets.Image(
-        #     window, (self.x, self.y), "images/BackOfCard.png"
-        # )
-        # self.showCard = False
+        self.cardName = f"{rank} of {suit}"
         self.imagesDict = {
             "frontOfCard": f"{rank} of {suit}.png",
             "backOfCard": "BackOfCard.png",
@@ -30,32 +22,21 @@ class Card:
         )
 
     def conceal(self):
-        # self.showCard = False
         self.currImage.replace("backOfCard")
 
     def setLoc(self, locTuple):
-        x, y = locTuple
-        # self.x = x
-        # self.y = y
-        # self.backOfCard.setLoc((self.x, self.y))
-        # self.frontOfCard.setLoc((self.x, self.y))
-        self.currImage.setLoc((x, y))
+        self.currImage.setLoc(locTuple)
 
     def reveal(self):
-        # self.showCard = True
         self.currImage.replace("frontOfCard")
 
     def getName(self):
-        return f"{self.rank} of {self.suit}"
+        return self.cardName
 
     def getValue(self):
         return self.value
 
     def draw(self):
-        # if self.showCard:
-        #     self.frontOfCard.draw()
-        # else:
-        #     self.backOfCard.draw()
         self.currImage.draw()
 
     # No need to change this
